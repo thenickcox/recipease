ba2dfd90b5010f90dccb343c6b56707b86043d82
thenickcox <nick@nickcox.me>
2013-04-06 20:48:26 -0700

    Config: don't initialize db on precompile

--- config/application.rb
+++ config/application.rb
@@ -64,6 +64,9 @@ module MichaelaRecipes
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Don't initialize db on precompile
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
