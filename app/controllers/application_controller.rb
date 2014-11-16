4ee5e150c4f87a597fa1839190abe5737b72ee58
Nick Cox <nick@nickcox.me>
2014-11-15 22:00:11 -0800

    another attempt at stopping Chrome from showing raw, cached JSON

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  before_filter :vary_accept
 
   def after_sign_in_path_for(resource)
     user_recipes_path(resource)
+  end
+
+  def vary_accept
+    response.headers["Vary"]= "Accept"
   end
 end
